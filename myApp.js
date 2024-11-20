c8442777ac2752a2d132c4fcea39fe16eb38ca64
Sabrina Ira <85188006+sabrinaira@users.noreply.github.com>
2024-11-20 00:07:55 -0500

    basic Node and Express challenge 5 checkpoint

--- myApp.js
+++ myApp.js
@@ -1,39 +1,23 @@
 let express = require('express');
 let app = express();
+let path = require('path');
 
+// console.log('Hello World');
 
+// app.get('/', (req, res) => {
+//   return res.send('Hello Express')
+// })
 
+app.get('/', (req, res) => {
+  let absolutePath = __dirname + '/views/index.html';
+  return res.status(200).sendFile(absolutePath);
+});
 
+let publicPath = __dirname + '/public';
+app.use('/public', express.static(publicPath));
 
+app.get('/json', (req, res) => {
+  res.json({ message: 'Hello json' });
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
